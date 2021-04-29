ec0c83387f857936d1c2e7b534bceb578ee8357c
James20EB <>
2021-04-29 13:51:14 +0000

    Nothing

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 public class Main {
+
+    public String text;
+    public Scanner Input;
+
     public static void main(String args[]){
-        System.out.println("test");
-        // I undid your change
+        try
+	    {
+		  input = new Scanner(file);
+	    }
+	  catch (FileNotFoundException ex)
+	  {
+		  System.out.println("*** Cannot open " + pathname + " ***");
+		  System.exit(1);  // quit the program
+	  } 
+
+    String[] array2 = new String[n];
+    int d = 0;
+    
+	  while(input.hasNext()){
+		  String word = input.next();
     }
 }
