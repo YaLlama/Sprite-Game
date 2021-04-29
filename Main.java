710921756fe4352ff66afcb7560331acbd029a79
YaLlama <47946874+YaLlama@users.noreply.github.com>
2021-04-29 14:13:29 -0400

    New code

--- Main.java
+++ Main.java
@@ -2,73 +2,87 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.io.IOException;
 
 public class Main {
 
-	public int width = 100;
-	public int height = 50;
-	public String[][] display = new String[width][height];
-  private static char input;
+  public static final int width = 100;
+  public static final int height = 50;
+  public static String[][] display = new String[width][height];
+  private static String userInput;
   private static boolean running = true;
+  private static File file;
+  private static Scanner inputTXT;
+  private static String fileName = "Input.txt";
+  //"C:\\Users\\jkcca\\IdeaProjects\\SpriteGame\\src\\Input.txt"
 
-	public static void main(String args[]){
-		//Sprite man = new Sprite("Sprites.txt");
+  public static void main(String args[]) {
 
-		checkwasd();
-	}
+    //initializing sprites
 
-
-
-  public static void checkwasd(){
-    File file= new File("Input.txt");
-    Scanner inputted = new Scanner(System.in);
-
-    try
-    {
-      inputted = new Scanner(file);
-    }
-    catch (FileNotFoundException ex)
-    {
-      System.out.println("*** Cannot open " + "pathname" + " ***");
-      System.exit(1);  // quit the program
-    }
+    //Sprite man = new Sprite("Sprites.txt");
 
 
-    try{
-      PrintWriter output = new PrintWriter(file);
-      while(running){
-        if(inputted.hasNext()){
-          input = inputted.next().charAt(0);
-          mainLoop();
-          output.print("");
+
+
+
+
+    //mainLoop
+
+    file = new File(fileName);
+
+    int fileLength = (int)file.length();
+
+    while (running) {
+      if(fileLength < file.length()){
+        fileLength = (int)file.length();
+        try{
+          inputTXT = new Scanner(file);
+          if (inputTXT.hasNextLine()) {
+            userInput = inputTXT.nextLine();
+            userInput = userInput.substring(userInput.length()-1);
+            mainLoop();
+          }
+        }catch(FileNotFoundException ex){
+          System.out.println("*** Cannot Read " + fileName + " ***");
+          System.exit(1);  // quit the program
         }
       }
+    }
+    try{
+      PrintWriter output = new PrintWriter(file);
+      output.print("");
       output.close();
-    }catch (FileNotFoundException ex){
-      System.out.println(" Cannot create " + "pathname" + " ");
+    }catch(FileNotFoundException ex){
+      System.out.println("*** Cannot Modify " + fileName + " ***");
       System.exit(1);  // quit the program
     }
+
+
+
+
+
+
+
+
+
+
+
+
   }
 
-
-  public static void mainLoop(){
-    if(input == 'q'){
+  private static void mainLoop(){
+    if(userInput == "q"){
       running = false;
     }
 
-    System.out.println("Input = " + input);
-  
+
+
+
+
+    System.out.println("Input = " + userInput);
+
   }
-
-
-
-
-
-
-
-
-
-
 
 
 }
